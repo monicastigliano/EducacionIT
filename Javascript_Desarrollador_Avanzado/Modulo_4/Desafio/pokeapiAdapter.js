d5b73abd195d851ebe3cc3db5da8d3cfc0b70c09
Monica Stigliano <monicastigliano@gmail.com>
2023-01-14 14:45:14 -0300

    Modulo 4 - Desafio falta resolver

---
+++ Javascript_Desarrollador_Avanzado/Modulo_4/Desafio/pokeapiAdapter.js
@@ -0,0 +1,66 @@
+/**
+ * Ejercicio 1:
+
+PokeApi es una API para obtener información sobre Pokemons. Además de ser
+divertida, es una API realmente compleja, con muchísimos recursos y relaciones. Es un
+excelente recurso para ir practicando llamadas AJAX.
+Siguiendo con nuestra idea de separar lo más posible las partes de nuestro proyecto,
+vamos a crear un Adaptador.
+Un Adaptador es una pieza de código encargada de comunicarse con algo externo (en
+este caso, PokeApi). Puede ser un archivo con varias funciones, por ejemplo.
+
+*En esta oportunidad, vamos a crear un archivo llamado PokeapiAdapter.js en donde vamos a
+poner únicamente una función: getFromPokeApi, que reciba la url del recurso a obtener y
+un callback, que será ejecutado con la respuesta cuando ésta esté disponible.
+
+ */
+
+function ajax(url, cb){
+    let xhr = new XMLHttpRequest()
+    xhr.open('get', url)
+    xhr.addEventListener('load', ()=>{
+        if(xhr.status === 200){
+            cb(xhr.response)
+            console.log
+        } else console.log(xhr.statusText);
+    })
+    xhr.send()
+}
+
+function getFromPokeApi(url, cb){
+    ajax(url, cb)
+}
+
+
+/**
+ * Ejercicio 2:
+ * 
+Una vez lograda esa función base, podemos desarrollar diferentes accesos o interfaces. En
+otras palabras, desarrollar funciones que faciliten usos específicos de esa función.
+Desarrolla las funciones getAllPokemons y getOnePokemon para obtener un listado de
+todos los pokemons, y el detalle de uno solo, respectivamente.
+
+ */
+
+
+function getAllPokemons(callback)
+{
+    getFromPokeApi('https://pokeapi.co/api/v2/', callback);
+}
+
+function getOnePokemon(id, callback)
+{
+    getFromPokeApi(`pokemon/${id}/`, function(pokemon){
+        callback({
+            nombre: pokemon.name,
+            peso: pokemon.weight,
+            altura: pokemon.height,
+            imc: pokemon.weight / pokemon.height
+        });
+    });
+}
+
+
+getOnePokemon(12, function(poke){
+    console.log(poke);
+});
